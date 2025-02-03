4848a4e5c9b83fc978485ded6b04651e09e60e8a
jorfigrom <jf26012@gmail.com>
2025-02-03 14:58:57 +0100

    Conexion mongodb, onboarding completo

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -61,8 +61,8 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     if (hasImageChanged) {
       const imgRes = await startUpload(files);
 
-      if (imgRes && imgRes[0].fileUrl) {
-        values.profile_photo = imgRes[0].fileUrl;
+      if (imgRes && imgRes[0].url ) { //url era fileUrl
+        values.profile_photo = imgRes[0].url;
       }
     }
 
@@ -155,7 +155,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
           render={({ field }) => (
             <FormItem className='flex w-full flex-col gap-3'>
               <FormLabel className='text-base-semibold text-light-2'>
-                Name
+                Nombre
               </FormLabel>
               <FormControl>
                 <Input
@@ -175,7 +175,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
           render={({ field }) => (
             <FormItem className='flex w-full flex-col gap-3'>
               <FormLabel className='text-base-semibold text-light-2'>
-                Username
+                Ususario
               </FormLabel>
               <FormControl>
                 <Input
@@ -195,7 +195,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
           render={({ field }) => (
             <FormItem className='flex w-full flex-col gap-3'>
               <FormLabel className='text-base-semibold text-light-2'>
-                Bio
+                Biografía
               </FormLabel>
               <FormControl>
                 <Textarea
