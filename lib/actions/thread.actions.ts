6b94f5db07d9b9382b227749facafce4cdf70029
jorfigrom <jf26012@gmail.com>
2025-02-26 12:15:08 +0100

    Card y comentarios, commit atrasado

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -5,62 +5,138 @@ import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
 interface Params {
-    text: string;
-    author: string;
-    communityId: string | null;
-    path: string;
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
 }
 
 export async function createThread({ text, author, communityId, path }: Params) {
 
-    try {
-        connectToDB();
+  try {
+    connectToDB();
 
-        const createdThread = await Thread.create({
-            text,
-            author,
-            community: null,
-        });
+    const createdThread = await Thread.create({
+      text,
+      author,
+      community: null,
+    });
 
-        //Update user model
-        await User.findByIdAndUpdate(author, {
-            $push: { threads: createdThread._id }
-        });
+    //Update user model
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id }
+    });
 
-        revalidatePath(path);
-    } catch (error: any) {
-        throw new Error(`Error al crear el thread: ${error.message}`);
-    }
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error al crear el thread: ${error.message}`);
+  }
 
 
 }
 
 export async function fetchPosts(pageNumber = 1, pageSize = 20) {
-    connectToDB();
-  
-    // Calculate the number of posts to skip based on the page number and page size.
-    const skipAmount = (pageNumber - 1) * pageSize;
-  
-    // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
-    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
-      .sort({ createdAt: "desc" })
-      .skip(skipAmount)
-      .limit(pageSize)
+  connectToDB();
+
+  // Calculate the number of posts to skip based on the page number and page size.
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: "author",
+      model: User,
+    })
+
+
+  // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  }); // Get the total count of posts
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+  connectToDB();
+
+
+  //comentarios de los comentarios
+  try {
+    const thread = await Thread.findById(id)
       .populate({
         path: "author",
         model: User,
+        select: "_id id name image"
       })
-      
-  
-    // Count the total number of top-level posts (threads) i.e., threads that are not comments.
-    const totalPostsCount = await Thread.countDocuments({
-      parentId: { $in: [null, undefined] },
-    }); // Get the total count of posts
-  
-    const posts = await postsQuery.exec();
-  
-    const isNext = totalPostsCount > skipAmount + posts.length;
-  
-    return { posts, isNext };
+      .populate({
+        path: 'children',
+        populate: [
+          {
+            path: 'author',
+            model: User,
+            select: '_id id name image'
+          },
+          {
+            path: 'children',
+            model: Thread,
+            populate: {
+              path: 'author',
+              model: User,
+              select: '_id id name image'
+            }
+          }
+        ]
+      }).exec();
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Error al buscar el thread: ${error.message}`);
   }
 
+}
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  connectToDB();
+
+  try {
+    // Encuentra el thread original por su id
+    const originalThread = await Thread.findById(threadId);
+
+    if (!originalThread) {
+      throw new Error("Thread not found");
+    }
+
+    // crea el comentario
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId, // establece el padre del hilo
+    });
+
+    // guarda el comentario
+    const savedCommentThread = await commentThread.save();
+
+    // añade elñ comentario como hijo del hilo
+    originalThread.children.push(savedCommentThread._id);
+
+    // guarda el hilo original con el nuevo comentario
+    await originalThread.save();
+
+    revalidatePath(path);
+  } catch (err) {
+    console.error("Error while adding comment:", err);
+    throw new Error("Unable to add comment");
+  }
+}
