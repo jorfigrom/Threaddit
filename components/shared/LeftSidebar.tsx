9b44cacc3263e56668b246e636db71e20811ead1
jorfigrom <jf26012@gmail.com>
2025-02-28 13:37:47 +0100

    funciona perfil

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -4,20 +4,30 @@ import Link from "next/link";
 import { sidebarLinks } from "@/constants";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, SignOutButton, useAuth } from "@clerk/nextjs";
 
 function LeftSidebar() {
 
     const router = useRouter();
     const pathname = usePathname();
 
+    const { userId } = useAuth();
+    //console.log(userId);
+
     return (
       <section className="custom-scrollbar leftsidebar">
         <div className="flex w-full flex-1 flex-col gap-6 px-6">
           {sidebarLinks.map((link) => {
 
-            const isActive = (pathname.includes
-              (link.route) && link.route.length > 1) || pathname === link.route;
+            const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+            
+            if (link.route === '/profile' && userId) {
+              link.route = `/profile/${userId}`;
+            }
+            
+            
+            
+
 
             return (
               <Link 
