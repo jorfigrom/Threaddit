124ab2eb264d303b924f5f7814b6e150552cfa89
jorfigrom <jf26012@gmail.com>
2025-03-02 19:01:19 +0100

    busquedas y paginacion

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,11 @@
+
+const Page = async () => {
+    return (
+        <section>
+            <h1 className="head-text mb-10"> Actividad </h1>
+        </section>
+    )
+
+}
+
+export default Page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -64,7 +64,7 @@ export async function fetchUserPosts(userId: string) {
   try {
     connectToDB();
 
-    // Find all threads authored by the user with the given userId
+    // Busca todos las publicaciones del usuario
     const threads = await User.findOne({ id: userId }).populate({
       path: "threads",
       model: Thread,
@@ -87,7 +87,7 @@ export async function fetchUserPosts(userId: string) {
   }
 }
 
-// Almost similar to Thead (search + pagination) and Community (search + pagination)
+
 export async function fetchUsers({
   userId,
   searchString = "",
@@ -104,18 +104,18 @@ export async function fetchUsers({
   try {
     connectToDB();
 
-    // Calculate the number of users to skip based on the page number and page size.
+    // Calcular cuántos usuarios omitir en función del número de página y el tamaño de página.
     const skipAmount = (pageNumber - 1) * pageSize;
 
-    // Create a case-insensitive regular expression for the provided search string.
+    // búsquedas sin diferenciar mayúsculas de minúsculas.
     const regex = new RegExp(searchString, "i");
 
-    // Create an initial query object to filter users.
+    // objeto de consulta para filtrar los usuarios.
     const query: FilterQuery<typeof User> = {
-      id: { $ne: userId }, // Exclude the current user from the results.
+      id: { $ne: userId }, // Excluir al usuario actual de los resultados.
     };
 
-    // If the search string is not empty, add the $or operator to match either username or name fields.
+    // buscar por nombre de usuario o nombre real.
     if (searchString.trim() !== "") {
       query.$or = [
         { username: { $regex: regex } },
@@ -123,28 +123,30 @@ export async function fetchUsers({
       ];
     }
 
-    // Define the sort options for the fetched users based on createdAt field and provided sort order.
+    // orden
     const sortOptions = { createdAt: sortBy };
+
 
     const usersQuery = User.find(query)
       .sort(sortOptions)
       .skip(skipAmount)
       .limit(pageSize);
 
-    // Count the total number of users that match the search criteria (without pagination).
+    // Contar la cantidad total de usuarios filtrados
     const totalUsersCount = await User.countDocuments(query);
 
     const users = await usersQuery.exec();
 
-    // Check if there are more users beyond the current page.
+
     const isNext = totalUsersCount > skipAmount + users.length;
 
     return { users, isNext };
   } catch (error) {
-    console.error("Error fetching users:", error);
+    console.error("Error al obtener usuarios:", error);
     throw error;
   }
 }
+
 
 export async function getActivity(userId: string) {
   try {
