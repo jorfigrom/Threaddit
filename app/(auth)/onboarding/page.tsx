182347af037d49f438e375f4b80925b3f4f36352
jorfigrom <jf26012@gmail.com>
2025-03-10 18:28:02 +0100

    intento de finalizar onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -23,9 +23,9 @@ async function Page() {
 
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
-      <h1 className='head-text'>Onboarding</h1>
+      <h1 className='head-text'>Sobre ti</h1>
       <p className='mt-3 text-base-regular text-light-2'>
-        Complete your profile now, to use Threds.
+        Completa tu perfil ahora para usar Threaddit al completo!
       </p>
 
       <section className='mt-9 bg-dark-2 p-10'>
