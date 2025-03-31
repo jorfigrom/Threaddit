87a126241eb50d37ccf6319dd8409b8fc1bec3e1
jorfigrom <jf26012@gmail.com>
2025-03-31 13:07:00 +0200

    editar perfil usuario hecho

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -41,7 +41,7 @@ function ProfileHeader({
           </div>
         </div>
         {accountId === authUserId && type !== "Community" && (
-          <Link href='/onboarding'>
+          <Link href='/profile/edit'>
             <div className='flex cursor-pointer gap-3 rounded-lg bg-dark-3 px-4 py-2'>
               <Image
                 src='/assets/edit.svg'
