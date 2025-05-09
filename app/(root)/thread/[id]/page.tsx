7dbb22b41b838f66f315243f4ff385686a0c4abe
jorfigrom <jf26012@gmail.com>
2025-05-09 12:18:36 +0200

    error coemntarios solucionado

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -57,16 +57,17 @@ async function page({ params }: { params: { id: string } }) {
             <div className='mt-10'>
                 {thread.children.map((childItem: any) => (
                     <ThreadCard
-                    id={thread._id}
+                    key={childItem._id}
+                    id={childItem._id}
                     currentUserId={user.id}
-                    parentId={thread.parentId}
-                    content={thread.text}
-                    author={thread.author}
-                    community={thread.community}
-                    createdAt={thread.createdAt}
-                    comments={thread.children}
-                    imageThread={thread.imageThread}
-                    likes={thread.likes || []}
+                    parentId={childItem.parentId}
+                    content={childItem.text}
+                    author={childItem.author}
+                    community={childItem.community}
+                    createdAt={childItem.createdAt}
+                    comments={childItem.children}
+                    imageThread={childItem.imageThread}
+                    likes={childItem.likes || []}
                 />
                 ))}
             </div>
